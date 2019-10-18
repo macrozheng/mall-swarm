fe96c7aab0831c16817690e98ad6c7c48c3a4569
macro <zhenghong1991@126.com>
2019-10-18 14:16:40 +0800

    添加SpringCloud Zuul网关支持

---
+++ mall-gateway/src/main/java/com/macro/mall/MallGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.macro.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+    }
+
+}
