b4bdda05ad281148c7e9aab9c74c5f4e629089d6
macro <zhenghong1991@126.com>
2020-08-24 20:23:10 +0800

    网关白名单问题修复

--- mall-auth/src/main/java/com/macro/mall/auth/config/Oauth2ServerConfig.java
+++ mall-auth/src/main/java/com/macro/mall/auth/config/Oauth2ServerConfig.java
@@ -43,15 +43,15 @@ public class Oauth2ServerConfig extends AuthorizationServerConfigurerAdapter {
                 .secret(passwordEncoder.encode("123456"))
                 .scopes("all")
                 .authorizedGrantTypes("password", "refresh_token")
-                .accessTokenValiditySeconds(3600)
-                .refreshTokenValiditySeconds(86400)
+                .accessTokenValiditySeconds(3600*24)
+                .refreshTokenValiditySeconds(3600*24*7)
                 .and()
                 .withClient("portal-app")
                 .secret(passwordEncoder.encode("123456"))
                 .scopes("all")
                 .authorizedGrantTypes("password", "refresh_token")
-                .accessTokenValiditySeconds(3600)
-                .refreshTokenValiditySeconds(86400);
+                .accessTokenValiditySeconds(3600*24)
+                .refreshTokenValiditySeconds(3600*24*7);
     }
 
     @Override
