cf0d4ba8aa40ee9aa1b1d4c86346295ce207ecb8
macro <zhenghong1991@126.com>
2020-09-22 20:28:24 +0800

    Update SwaggerConfig.java

--- mall-search/src/main/java/com/macro/mall/search/config/SwaggerConfig.java
+++ mall-search/src/main/java/com/macro/mall/search/config/SwaggerConfig.java
@@ -2,14 +2,7 @@ package com.macro.mall.search.config;
 
 import com.macro.mall.common.config.BaseSwaggerConfig;
 import com.macro.mall.common.domain.SwaggerProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
