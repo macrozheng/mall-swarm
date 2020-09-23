049798b890b78ed1ffdd8f91770fc560fe7d0e31
macro <zhenghong1991@126.com>
2020-09-23 20:47:06 +0800

    Update SwaggerConfig.java

--- mall-portal/src/main/java/com/macro/mall/portal/config/SwaggerConfig.java
+++ mall-portal/src/main/java/com/macro/mall/portal/config/SwaggerConfig.java
@@ -2,22 +2,8 @@ package com.macro.mall.portal.config;
 
 import com.macro.mall.common.config.BaseSwaggerConfig;
 import com.macro.mall.common.domain.SwaggerProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Swagger API文档相关配置
