c66b4056c48eb96c9427f74ec33405618785b58e
macro <zhenghong1991@126.com>
2020-10-14 21:13:16 +0800

    Update EsProductRelatedInfo.java

--- mall-search/src/main/java/com/macro/mall/search/domain/EsProductRelatedInfo.java
+++ mall-search/src/main/java/com/macro/mall/search/domain/EsProductRelatedInfo.java
@@ -3,7 +3,7 @@ package com.macro.mall.search.domain;
 import java.util.List;
 
 /**
- * 搜索相关商品品牌名称，分类名称及属性
+ * 搜索商品的品牌名称，分类名称及属性
  * Created by macro on 2018/6/27.
  */
 public class EsProductRelatedInfo {
